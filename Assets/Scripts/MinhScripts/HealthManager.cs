9947e9c2272f327a2a4e950311a6dbbff9a82460
ZetexGGs <114045259+ZetexGGs@users.noreply.github.com>
2025-03-25 10:01:56 -0500

    Health Bar
    I began working on the health bar. I am not finished. I will work more when I get home.

---
+++ Assets/Scripts/MinhScripts/HealthManager.cs
@@ -0,0 +1,40 @@
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    public Image healthBar;
+    public float healthAmount = 100f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            TakeDamage(20);
+        }  
+
+        if(Input)
+    }
+
+    public void TakeDamage(float damage)
+    {
+        healthAmount -= damage;
+        healthBar.fillAmount = healthAmount / 100f;
+    }
+
+    public void Heal(float healingAmount)
+    {
+        healthAmount += healingAmount;
+        healthAmount = Mathf.Clamp(healthAmount, 0, 100);
+
+        healthBar.fillAmount = healthAmount / 100f;
+    }
+}
